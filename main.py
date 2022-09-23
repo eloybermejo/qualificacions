033633aa2475f2935599fc363d29c0eab91b5a9e
NicolasDiazVazquez <nicolasdiaz@inspladelestany.cat>
2022-09-23 17:42:19 +0200

    test

--- main.py
+++ main.py
@@ -1,54 +1,29 @@
-# Eloy Bermejo i Nico Diaz
 # Definició de classes
-# Definició de classes
-# Definició de classes
-class UnitatFormativa:
-    nom = None
-    qualificacio = None
-    hores = None
-
-    def __init__(self, nom, hores):
-        self.nom = nom
-        self.hores = hores
+from modulprofessional import ModulProfessional
+from unitatformativa import UnitatFormativa
 
 
-class ModulProfessional:
-    nom = None
-    unitats_formatives = []
+def inicialitzar_modul_professional():
+    global uf1, uf2, uf3, mp5
+    uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
+    uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
+    uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
+    uf1.qualificacio = 8
+    uf2.qualificacio = 10
+    uf3.qualificacio = 4
+    mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
+    mp5.afegir_unitat_formativa(uf1)
+    mp5.afegir_unitat_formativa(uf2)
+    mp5.afegir_unitat_formativa(uf3)
 
-    def __init__(self, nom):
-        self.nom = nom
 
-    def afegir_unitat_formativa(self, unitat_formativa):
-        self.unitats_formatives.append(unitat_formativa);
-
-    def getQualificacio(self):
-        suma_hores = 0
-        suma_qualificacio = 0
-
-        for uf in self.unitats_formatives:
-            suma_hores += uf.hores
-            suma_qualificacio += (uf.qualificacio * uf.hores)
-
-        return suma_qualificacio / suma_hores
+def mostrar_dades_modul_professional():
+    print(uf1.nom, ":", uf1.qualificacio)
+    print(uf2.nom, ":", uf2.qualificacio)
+    print(uf3.nom, ":", uf3.qualificacio)
+    print(mp5.nom, ":", mp5.get_qualificacio())
 
 
 # Inici del programa
-uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
-uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
-uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
-
-uf1.qualificacio = 8
-uf2.qualificacio = 10
-uf3.qualificacio = 4
-
-mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
-
-mp5.afegir_unitat_formativa(uf1)
-mp5.afegir_unitat_formativa(uf2)
-mp5.afegir_unitat_formativa(uf3)
-
-print(uf1.nom, ":", uf1.qualificacio)
-print(uf2.nom, ":", uf2.qualificacio)
-print(uf3.nom, ":", uf3.qualificacio)
-print(mp5.nom, ":", mp5.getQualificacio())
+inicialitzar_modul_professional()
+mostrar_dades_modul_professional()
